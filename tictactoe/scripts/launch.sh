6a4e868fcb8b168f59d185c2b551d0c164774b15
Thomas Holloway <nyxtom@gmail.com>
2011-04-26 02:43:00 -0400

    Modified launch to also include the runserver

--- tictactoe/scripts/launch.sh
+++ tictactoe/scripts/launch.sh
@@ -5,13 +5,23 @@ yellow="\033[1;33m"
 white="\033[1;37m"
 nc="\033[0m"
 
+silent () {
+    ($@ 2>&1 > /dev/null) 2>&1 > /dev/null
+}
+
+simplekill () {
+    silent kill -9 `ps aux | egrep -i $1 | egrep -v "(grep)" | awk '{ print $2 }'`
+}
+
 if [ -e supervisord.conf ]; then
     printf $green
     echo "Starting up..."
-    printf $nc
-
+    simplekill 'supervisor(d|ctl)'
+    silent supervisord -c supervisord.conf
     mkdir -p logs
-    supervisord -c supervisord.conf -n
+
+    printf $nc
+    python ../manage.py runserver $@
 else
     printf $red
     echo "Could not find supervisord.conf"
